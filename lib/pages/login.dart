7796cd10897cb21c8e9e9a9d53d56fc93adc2aab
Bhargavi <bhargavi.kurukunda@students.iiit.ac.in>
2022-02-05 15:14:21 +0530

    linked to firebase data

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -317,29 +317,54 @@ class _LoginScreenState extends State<LoginScreen> {
   // Login function
   void SignIn(String email, String password) async {
     if (_formKey.currentState!.validate()) {
-      await _auth
-          .signInWithEmailAndPassword(email: email, password: password)
-          .then((uid) => {
-                Fluttertoast.showToast(msg: "Login Successfull !"),
-                if (patient)
-                  {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (context) => patientHomeScreen())),
-                  },
-                if (doctor)
-                  {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (context) => doctorHomeScreen())),
-                  },
-                if (admin)
-                  {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (context) => adminHomeScreen())),
-                  },
-              })
-          .catchError((e) {
-        Fluttertoast.showToast(msg: e!.message);
-      });
+      try {
+        await _auth
+            .signInWithEmailAndPassword(email: email, password: password)
+            .then((uid) => {
+                  Fluttertoast.showToast(msg: "Login Successfull !"),
+                  if (patient)
+                    {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => patientHomeScreen())),
+                    },
+                  if (doctor)
+                    {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => doctorHomeScreen())),
+                    },
+                  if (admin)
+                    {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => adminHomeScreen())),
+                    },
+                });
+      } on FirebaseAuthException catch (error) {
+        switch (error.code) {
+          case "invalid-email":
+            errorMessage = "Your email address appears to be malformed.";
+
+            break;
+          case "wrong-password":
+            errorMessage = "Your password is wrong.";
+            break;
+          case "user-not-found":
+            errorMessage = "User with this email doesn't exist.";
+            break;
+          case "user-disabled":
+            errorMessage = "User with this email has been disabled.";
+            break;
+          case "too-many-requests":
+            errorMessage = "Too many requests";
+            break;
+          case "operation-not-allowed":
+            errorMessage = "Signing in with Email and Password is not enabled.";
+            break;
+          default:
+            errorMessage = "An undefined Error happened.";
+        }
+        Fluttertoast.showToast(msg: errorMessage!);
+        print(error.code);
+      }
     }
   }
 }
