3b3f2fc3ff6c5b37ad6c1cafc437c323abad1033
Ananya_sudi <79156696+ananyasudi@users.noreply.github.com>
2022-03-11 20:39:42 +0530

    changed homepage_patient.dart to profile.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,7 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import '../pages/register.dart';
-import '../pages/homepage_patient.dart';
+import '../pages/profile.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
