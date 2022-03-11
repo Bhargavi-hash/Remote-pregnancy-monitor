070e2f6167f735c0465859d5d1e685bf3553eeca
Ananya_sudi <79156696+ananyasudi@users.noreply.github.com>
2022-03-11 21:28:39 +0530

    added curved_nav_bar

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -7,6 +7,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart' as http;
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import './vitals_input.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
@@ -22,6 +24,8 @@ class _patientHomeScreenState extends State<patientHomeScreen> {
   String _email = "";
   String _phoneNumber = "";
   final FirebaseAuth auth = FirebaseAuth.instance;
+  int navigationIndex = 0;
+  GlobalKey globalKey = new GlobalKey(debugLabel: 'btm_app_bar');
 
   @override
   _fetch() async {
@@ -32,37 +36,69 @@ class _patientHomeScreenState extends State<patientHomeScreen> {
           .doc(firebaseUser.uid)
           .get()
           .then((ds) {
-            setState(() {
-            _name=ds.data()!['fullName'];
-            _email=ds.data()!['email'];
-            _phoneNumber=ds.data()!['phoneNumber'];
-              
-            });
-            
+        setState(() {
+          _name = ds.data()!['fullName'];
+          _email = ds.data()!['email'];
+          _phoneNumber = ds.data()!['phoneNumber'];
+        });
 
         print(ds.data());
         print('$_email,$_phoneNumber');
       });
     }
   }
-  void initState(){
+
+  void initState() {
     _fetch();
   }
 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.pink,
+          items: <Widget>[
+            Icon(
+              Icons.person,
+              size: 30,
+              color: Colors.pink,
+            ),
+            Icon(
+              Icons.list_alt_outlined,
+              size: 30,
+              color: Colors.pink,
+            ),
+            Icon(
+              Icons.search,
+              size: 30,
+              color: Colors.pink,
+            ),
+          ],
+          onTap: (index) {
+            //Handle button tap
+            // setState(() {
+            //   navigationIndex = index;
+            switch (index) {
+              case 1:
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => vital_parameters_screen()));
+                break;
+            }
+            // });
+          },
+        ),
         body: SafeArea(
             child: Center(
           child: Column(
             children: <Widget>[
               SizedBox(
-                height: 200.0,
-                child:Image(
-                  image:AssetImage('assets/logo.png'),
-                  fit: BoxFit.contain,
-                )
-              ),
+                  height: 200.0,
+                  child: Image(
+                    image: AssetImage('assets/logo.png'),
+                    fit: BoxFit.contain,
+                  )),
               Text(
                 'Your Profile',
                 style: TextStyle(
